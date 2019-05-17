a1a75cd0ee95d6bbedc9433c4060bf1ec239be54
Artem Kostiuk <postatum@gmail.com>
2019-05-17 06:14:20 +0300

    Fix webapi-parser java runner

--- java/src/main/java/org/raml/parsers/WebApiParser.java
+++ java/src/main/java/org/raml/parsers/WebApiParser.java
@@ -1,7 +1,7 @@
 package org.raml.parsers;
 
 import webapi.Raml10;
-import amf.client.model.document.BaseUnit;
+import webapi.WebApiBaseUnit;
 import amf.client.validate.ValidationReport;
 import amf.client.validate.ValidationResult;
 
@@ -12,7 +12,7 @@ import java.util.List;
 // https://github.com/raml-org/webapi-parser
 public class WebApiParser implements IParser {
   public void parse(String fpath) throws InterruptedException, ExecutionException, IllegalArgumentException {
-    final BaseUnit model = Raml10.parse("file://" + fpath).get();
+    final WebApiBaseUnit model = Raml10.parse("file://" + fpath).get();
     final ValidationReport report = Raml10.validate(model).get();
     final List<ValidationResult> results = report.results();
     for (ValidationResult res : results) {
